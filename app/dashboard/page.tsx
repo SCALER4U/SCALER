d10f3fdf5f5515a794f1329ef2f1b76a712d25f0
SCALER4U <karkar.hussein@gmail.com>
2026-02-01 17:29:59 +0400

    Create shopify

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+      <p>This is your marketing software.</p>
+      <button>Connect Shopify</button>
+    </div>
+  );
+}
